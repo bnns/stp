a3d342110b976ca557dc8e98f354018b494b82d1
Dennis Yao <omethyn@gmail.com>
2022-01-20 23:15:03 -0600

    add cdn to the upload plugin

--- config/plugins.js
+++ config/plugins.js
@@ -7,6 +7,8 @@ module.exports = ({ env }) => ({
         secret: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
         space: env("DO_SPACE_BUCKET"),
+        directory: env("DO_SPACE_DIRECTORY"),
+        cdn: env("DO_SPACE_CDN"),
       },
     },
   },
