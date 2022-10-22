5db60e86543270da447a3236adf6ddd05f9a1848
Dennis Yao <omethyn@gmail.com>
2022-10-22 16:56:52 -0500

    SES integration

--- config/plugins.js
+++ config/plugins.js
@@ -12,4 +12,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: env('AWS_SES_KEY'),
+        secret: env('AWS_SES_SECRET'),
+        amazon: 'https://email.us-east-1.amazonaws.com',
+      },
+      settings: {
+        defaultFrom: 'SubsetOfTheoreticalPractice@gmail.com',
+        defaultReplyTo: 'SubsetOfTheoreticalPractice@gmail.com',
+      },
+    },
+  },
 });
